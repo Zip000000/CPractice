03025336e64fd803510ee1e50dd3e934984b608b
Zip <307110017@qq.com>
2019-04-30 20:50:25 +0800

    change

--- Class_Code/_test.c
+++ Class_Code/_test.c
@@ -7,74 +7,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define ERROR 0
-#define OK 1
-
 typedef struct Vector {
-    int length,size;
+    int length, size;
     int* data;
-} Vector;
+}Vector;
 
-void init(Vector* vector,int _size) {
+Vector* Vec_Init(int _size) {
+    Vector* vector;
+    vector = (Vector* )malloc(sizeof(Vector));
     vector->size = _size;
     vector->length = 0;
-    vector->data = (int* )malloc(sizeof(int)*_size);
+    vector->data = (int* )malloc(sizeof(int) * vector->size);
+    return vector;
 }
 
-void expand(Vector* vector) {  //2倍
-	int* old_data = vector->data;
-    vector->size *= 2;
-    vector->data = (int* ) malloc(sizeof(int) * vector->size);
-    for(int i=0; i<vector->length; i++)
-    {
-        vector->data[i] = old_data[i];
+void show(Vector* vector) {
+    printf("开始输出：\n");
+    for(int i = 0; i < vector->length; i++) {
+        i == 0 || printf(" ");
+        printf("%d", vector->data[i]);
     }
-    free(old_data);
-}
-
-void insert(Vector* vector,int loc,int value) {
-    printf("进入insert\n");
-    if(loc > vector->length)
-    {
-        printf("failed\n");
-        return;
-    }
-    
-    if(vector->length >= vector->size)
-    {
-        printf("expand\n");
-        expand(vector);
-        printf("expand successful!\n");
-    }
-    
-    for(int i=vector->length;i>loc;i--)
-    {
-        vector->data[i] = vector->data[i-1]; 
-    }
-	vector->data[loc]=value;
-    vector->length++;
-    printf("success\n");
-}
-
-
-void clear(Vector* vector) {
-	free(vector->data);
-	free(vector);
 }
 
 int main() {
-    Vector *a = (Vector *)malloc(sizeof(Vector));
-    init(a, 5);
-    int n;
-    scanf("%d",&n);
-    int p,q;
-    for(int i=0;i<n;i++) {
-        scanf("%d%d", &p, &q);
-        printf("输入成功%d\n", i);
-        insert(a, p, q);
-        printf("size=%d length=%d \n", a->size, a->length);
+    int n, k;
+    scanf("%d %d", &n, &k);
+    Vector* vector;
+    vector = Vec_Init(n);
+    for(int i = 0; i < n; i++) {
+        scanf("%d", &vector->data[i]);
     }
-    
-
+    show(vector);
     return 0;
 }
