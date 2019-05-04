45bad9e4d2e8cd2497049ed2ee1f4a23eb2650ec
Zip <307110017@qq.com>
2019-05-04 21:08:58 +0800

    upload new code and MY_NOTE

--- 1.Class_Code/_test.c
+++ 1.Class_Code/_test.c
@@ -6,37 +6,8 @@
  ************************************************************************/
 #include <stdio.h>
 #include <stdlib.h>
-
-typedef struct Vector {
-    int length, size;
-    int* data;
-}Vector;
-
-Vector* Vec_Init(int _size) {
-    Vector* vector;
-    vector = (Vector* )malloc(sizeof(Vector));
-    vector->size = _size;
-    vector->length = 0;
-    vector->data = (int* )malloc(sizeof(int) * vector->size);
-    return vector;
-}
-
-void show(Vector* vector) {
-    printf("开始输出：\n");
-    for(int i = 0; i < vector->length; i++) {
-        i == 0 || printf(" ");
-        printf("%d", vector->data[i]);
-    }
-}
-
+#include <string.h>
 int main() {
-    int n, k;
-    scanf("%d %d", &n, &k);
-    Vector* vector;
-    vector = Vec_Init(n);
-    for(int i = 0; i < n; i++) {
-        scanf("%d", &vector->data[i]);
-    }
-    show(vector);
+    printf("%d\n", strcmp("bcdfhdsjhfjsdkhfdsjK", "abcd"));
     return 0;
 }
