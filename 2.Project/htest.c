abe61aff1e02526563cafdf7c78be4cc724faddf
Zip <307110017@qq.com>
2019-05-19 15:18:46 +0800

    save

--- 2.Project/htest.c
+++ 2.Project/htest.c
@@ -21,6 +21,22 @@ TEST(test, add) {
     EXPECT_EQ(add(10, 200), 30);
 }
 
+TEST(test2, add2) {
+    EXPECT_EQ(add(1, 2), 3);
+    EXPECT_EQ(add(10, 20), 30);
+    EXPECT_EQ(add(1000, 2000), 3000);
+    EXPECT_EQ(add(10000, 20000), 30000);
+    EXPECT_EQ(add(100, 200), 300);
+}
+
+TEST(test3, add3) {
+    EXPECT_NE(add(1, 2), 3);
+    EXPECT_NE(add(10, 20), 30);
+    EXPECT_NE(add(1000, 2000), 3000);
+    EXPECT_NE(add(10000, 20000), 30000);
+    EXPECT_NE(add(100, 200), 300);
+}
+
 int main() {
     return RUN_ALL_TESTS();
 }
--- 2.Project/htest.h
+++ 2.Project/htest.h
@@ -36,31 +36,74 @@ __attribute__((constructor)) void FuncName(add_Test, __COUNTER__)() { \
     p->name_part2 = STR(b); \
     p->next = head; \
     head = p; \
+    TestNum++; \
 } \
 void FuncName(a, FuncName(_, FuncName(b, FuncName(_, Test))))()
-#define EXPECT_EQ(a, b) \
-printf("%s\n", a == b ? "\033[32m[    Yes    ]\033[0m" : "\033[31m[    N o    ]\033[0m"); \
+
+#define EXPECT(a,b,c) \
+        if(a c b) { \
+            add_right(); \
+        } else { \
+            printf("%s ","\033[33m[    WRONG ]\033[0m"); \
+            printf("%s = %s 输出结果：%d 正确结果：%d\n", #a, #b, a, b); \
+            add_wrong(); \
+           }
+
+#define EXPECT_EQ(a, b)  EXPECT(a, b, ==)
+
+        /*
         if(a != b) { \
-            printf("输出结果：%d\n正确结果：%d\n", a, b); \
-        } \
+            printf("%s ","\033[33m[    WRONG ]\033[0m"); \
+            printf("%s = %s 输出结果：%d 正确结果：%d\n", #a, #b, a, b); \
+            add_wrong(); \
+           } else { \
+            add_right(); \
+           } \
+*/
+#define EXPECT_NE(a, b) EXPECT(a, b, !=)
+#define EXPECT_GT(a, b) EXPECT(a, b, >)
+#define EXPECT_LT(a, b) EXPECT(a, b, <)
+#define EXPECT_LE(a, b) EXPECT(a, b, <=)
+#define EXPECT_GE(a, b) EXPECT(a, b, >=)
+static double allnum=0;
+static double wrongnum=0;
+static int TestNum=0;
+
+typedef struct info {
+    float rightnum;
+    float wrongnum;
+}Info;
+static Info info;
+void clear () {
+    info.rightnum = 0;
+    info.wrongnum = 0;
+}
+void add_right() {
+    info.rightnum++;
+}
+void add_wrong() {
+    info.wrongnum++;
+}
+#define COLOR(a,b) 
 
 int RUN_ALL_TESTS() {
-    int passnum=0;
-    int failnum=0;
+    clear();
     pFuncData p = head;
-        printf("[===========] TEST START!\n");
+        printf("[==========]\033[44;37m RUNNING %d TESTS, TEST START!\033[0m\n", TestNum);
     while (p != NULL) {
-
-        printf("\033[32m[ RUN       ]\033[0m %s.%s\n", p->name_part1, p->name_part2);
+        clear();
+        printf("\033[32m[----------]\033[0m %s.%s\n", p->name_part1, p->name_part2);
+        printf("\033[32m[ RUN      ]\033[0m %s.%s\n", p->name_part1, p->name_part2);
         p->func();
+        if(info.wrongnum==0 && info.rightnum!=0) printf("\033[32m[       OK ]\033[0m %s.%s\n", p->name_part1, p->name_part2);
+        else printf("\033[1;31m[  FAILED  ]\033[0m %s.%s\n", p->name_part1, p->name_part2);
+        float score = 100.0 * (info.rightnum / (info.rightnum+info.wrongnum));
+        if(score != 100) printf("\033[31m[  %.2f%%  ]\033[0m\n", score);
+        printf("\033[32m[----------]\033[0m %s.%s\n\n", p->name_part1, p->name_part2);
         p = p->next;
-        printf("[        OK ]\n");
     }
     return 0;
 }
-
-
-
 
 
 #endif
